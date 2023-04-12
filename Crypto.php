c7cf344ff35ec3422fdcaf30b3d93dacb35cc203
Amin Paya | امین پایا <89313828+rebelliume@users.noreply.github.com>
2023-04-12 11:26:50 -0800

    Algorithms Rebuilt
    Efficiency & Automation Added.

--- Crypto.php
+++ Crypto.php
@@ -49,147 +49,62 @@ class Crypto
         $privKey = openssl_pkey_get_private($private_key);
         openssl_pkey_export($privKey, $priKey);
         return array("pubKey" => $pubKey, "privKey" => $priKey);
-    }
-
+    }    
 
     public static function asymmetricEncryptPublic($data, $publicKey)
     {
-        $len = strlen($data);
-        if ($len > 1410) {
-            return;
+        if(strlen($data) > 214)
+        {            
+            openssl_public_encrypt(substr($data, 0, 214), $encrypted_data, $publicKey, OPENSSL_PKCS1_OAEP_PADDING);
+            $encrypted_data .= self::asymmetricEncryptPublic(substr($data, 214), $publicKey);            
         }
-        if ($len < 940) {
-            $data1 = substr($data, 0, 470);
-            $data2 = substr($data, 470);
-            openssl_public_encrypt($data1, $encrypted_data1, $publicKey, OPENSSL_PKCS1_OAEP_PADDING);
-            openssl_public_encrypt($data2, $encrypted_data2, $publicKey, OPENSSL_PKCS1_OAEP_PADDING);
-            return base64_encode($encrypted_data1 . $encrypted_data2);
-        } elseif ($len > 940) {
-            $data1 = substr($data, 0, 470);
-            $data2 = substr($data, 470, 470);
-            $data3 = substr($data, 940, 470);
-            openssl_public_encrypt($data1, $encrypted_data1, $publicKey, OPENSSL_PKCS1_OAEP_PADDING);
-            openssl_public_encrypt($data2, $encrypted_data2, $publicKey, OPENSSL_PKCS1_OAEP_PADDING);
-            openssl_public_encrypt($data3, $encrypted_data3, $publicKey, OPENSSL_PKCS1_OAEP_PADDING);
-            return base64_encode($encrypted_data1 . $encrypted_data2 . $encrypted_data3);
-        } else {
+        else {
             openssl_public_encrypt($data, $encrypted_data, $publicKey, OPENSSL_PKCS1_OAEP_PADDING);
-            return base64_encode($encrypted_data);
         }
-    }
-
-    public static function asymmetricDecryptPrivate($data, $privateKey)
-    {
-        $len = strlen($data);
-        $data = base64_decode($data);
-        if ($len == 684) {
-            $data1 = substr($data, 0, 512);
-            $data2 = substr($data, 512);
-            openssl_private_decrypt($data1, $decrypted_data1, $privateKey, OPENSSL_PKCS1_OAEP_PADDING);
-            openssl_private_decrypt($data2, $decrypted_data2, $privateKey, OPENSSL_PKCS1_OAEP_PADDING);
-            return $decrypted_data1 . $decrypted_data2;
-        } elseif ($len == 1368) {
-            $data1 = substr($data, 0, 512);
-            $data2 = substr($data, 512, 512);
-            $data3 = substr($data, 1024, 344);
-            openssl_private_decrypt($data1, $decrypted_data1, $privateKey, OPENSSL_PKCS1_OAEP_PADDING);
-            openssl_private_decrypt($data2, $decrypted_data2, $privateKey, OPENSSL_PKCS1_OAEP_PADDING);
-            openssl_private_decrypt($data3, $decrypted_data3, $privateKey, OPENSSL_PKCS1_OAEP_PADDING);
-            return $decrypted_data1 . $decrypted_data2 . $decrypted_data3;
-        } elseif ($len == 2048) {
-            $data1 = substr($data, 0, 512);
-            $data2 = substr($data, 512, 512);
-            $data3 = substr($data, 1024, 512);
-            $data4 = substr($data, 1536, 512);
-            openssl_private_decrypt($data1, $decrypted_data1, $privateKey, OPENSSL_PKCS1_OAEP_PADDING);
-            openssl_private_decrypt($data2, $decrypted_data2, $privateKey, OPENSSL_PKCS1_OAEP_PADDING);
-            openssl_private_decrypt($data3, $decrypted_data3, $privateKey, OPENSSL_PKCS1_OAEP_PADDING);
-            openssl_private_decrypt($data4, $decrypted_data4, $privateKey, OPENSSL_PKCS1_OAEP_PADDING);
-            return $decrypted_data1 . $decrypted_data2 . $decrypted_data3 . $decrypted_data4;
-        }
-    }
-
-    public static function asymmetricEncryptPrivate($data, $privateKey)
-    {
-        $len = strlen($data);
-        if ($len > 2048) {
-            return;
-        }
-        if ($len <= 684) {
-            $data1 = substr($data, 0, 500);
-            $data2 = substr($data, 500, 184);
-            openssl_private_encrypt($data1, $encrypted_data1, $privateKey);
-            openssl_private_encrypt($data2, $encrypted_data2, $privateKey);
-            return base64_encode($encrypted_data1 . $encrypted_data2);
-        } elseif ($len == 1368) {
-            $data1 = substr($data, 0, 500);
-            $data2 = substr($data, 500, 500);
-            $data3 = substr($data, 1000, 368);
-            openssl_private_encrypt($data1, $encrypted_data1, $privateKey);
-            openssl_private_encrypt($data2, $encrypted_data2, $privateKey);
-            openssl_private_encrypt($data3, $encrypted_data3, $privateKey);
-            return base64_encode($encrypted_data1 . $encrypted_data2 . $encrypted_data3);
-        } elseif ($len == 2048) {
-            $data1 = substr($data, 0, 500);
-            $data2 = substr($data, 500, 500);
-            $data3 = substr($data, 1000, 500);
-            $data4 = substr($data, 1500, 500);
-            $data5 = substr($data, 2000, 48);
-            openssl_private_encrypt($data1, $encrypted_data1, $privateKey);
-            openssl_private_encrypt($data2, $encrypted_data2, $privateKey);
-            openssl_private_encrypt($data3, $encrypted_data3, $privateKey);
-            openssl_private_encrypt($data4, $encrypted_data4, $privateKey);
-            openssl_private_encrypt($data5, $encrypted_data5, $privateKey);
-            return base64_encode($encrypted_data1 . $encrypted_data2 . $encrypted_data3 . $encrypted_data4 . $encrypted_data5);
-        } else {
-            return false;
-        }
+        return base64_encode($encrypted_data);
     }
 
     public static function asymmetricDecryptPublic($data, $publicKey): bool|string
     {
-        $len = strlen($data);
-        $data = base64_decode($data);
-        if ($len > 3416) {
-            return false;
+        if (base64_decode($data, true) === true) { $data = base64_decode($data); }
+
+        if(strlen($data) > 512)
+        {            
+            openssl_public_decrypt(substr($data, 0, 512), $decrypted_data, $publicKey, OPENSSL_PKCS1_OAEP_PADDING);
+            $decrypted_data .= self::asymmetricDecryptPublic(substr($data, 512), $publicKey);            
         }
-        if ($len == 1368) {
-            $data1 = substr($data, 0, 512);
-            $data2 = substr($data, 512, 512);
-            $data3 = substr($data, 1024, 344);
-            openssl_public_decrypt($data1, $encrypted_data1, $publicKey);
-            openssl_public_decrypt($data2, $encrypted_data2, $publicKey);
-            openssl_public_decrypt($data3, $encrypted_data3, $publicKey);
-            return $encrypted_data1 . $encrypted_data2 . $encrypted_data3;
-        } elseif ($len == 2048) {
-            $data1 = substr($data, 0, 512);
-            $data2 = substr($data, 512, 512);
-            $data3 = substr($data, 1024, 512);
-            $data4 = substr($data, 1536, 512);
-            openssl_public_decrypt($data1, $encrypted_data1, $publicKey);
-            openssl_public_decrypt($data2, $encrypted_data2, $publicKey);
-            openssl_public_decrypt($data3, $encrypted_data3, $publicKey);
-            openssl_public_decrypt($data4, $encrypted_data4, $publicKey);
-            return $encrypted_data1 . $encrypted_data2 . $encrypted_data3 . $encrypted_data4;
-        } elseif ($len == 3416) {
-            $data1 = substr($data, 0, 512);
-            $data2 = substr($data, 512, 512);
-            $data3 = substr($data, 1024, 512);
-            $data4 = substr($data, 1536, 512);
-            $data5 = substr($data, 2048, 512);
-            $data6 = substr($data, 2560, 512);
-            $data7 = substr($data, 3072, 344);
-            openssl_public_decrypt($data1, $encrypted_data1, $publicKey);
-            openssl_public_decrypt($data2, $encrypted_data2, $publicKey);
-            openssl_public_decrypt($data3, $encrypted_data3, $publicKey);
-            openssl_public_decrypt($data4, $encrypted_data4, $publicKey);
-            openssl_public_decrypt($data5, $encrypted_data5, $publicKey);
-            openssl_public_decrypt($data6, $encrypted_data6, $publicKey);
-            openssl_public_decrypt($data7, $encrypted_data7, $publicKey);
-            return $encrypted_data1 . $encrypted_data2 . $encrypted_data3 . $encrypted_data4 . $encrypted_data5 . $encrypted_data6 . $encrypted_data7;
-        } else {
-            return false;
+        else {            
+            openssl_public_decrypt($data, $decrypted_data, $publicKey, OPENSSL_PKCS1_OAEP_PADDING);
         }
+        return $decrypted_data;
+    }
+
+    public static function asymmetricEncryptPrivate($data, $privateKey)
+    {
+        if(strlen($data) > 214)
+        {            
+            openssl_private_encrypt(substr($data, 0, 214), $encrypted_data, $privateKey, OPENSSL_PKCS1_OAEP_PADDING);
+            $encrypted_data .= self::asymmetricEncryptPrivate(substr($data, 214), $privateKey);
+        }
+        else {
+            openssl_private_encrypt($data, $encrypted_data, $privateKey, OPENSSL_PKCS1_OAEP_PADDING);            
+        }
+        return base64_encode($encrypted_data);
+    }
+
+    public static function asymmetricDecryptPrivate($data, $privateKey)
+    {
+        if (base64_decode($data, true) === true) { $data = base64_decode($data); }
+
+        if(strlen($data) > 512)
+        {            
+            openssl_private_decrypt(substr($data, 0, 512), $decrypted_data, $privateKey, OPENSSL_PKCS1_OAEP_PADDING);
+            $decrypted_data .= self::asymmetricDecryptPrivate(substr($data, 512), $privateKey);            
+        }
+        else {            
+            openssl_private_decrypt($data, $decrypted_data, $privateKey, OPENSSL_PKCS1_OAEP_PADDING);
+        }
+        return $decrypted_data;
     }
 
     public function __call(string $name, array $arguments)
